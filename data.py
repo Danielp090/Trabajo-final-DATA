43c4dcd9cc517e509340d0b53acf092385c988c8
¨Danielp090¨ <daniel.peralta@es.uazuay.edu.ec>
2025-05-31 11:01:32 -0500

    Renombrar columnas y tratar los valores atipicos del dataset

--- data.py
+++ data.py
@@ -8,4 +8,29 @@ df= pd.read_csv(url)
 df.dropna(inplace=True)
 #eliminacion de duplicados
 df.drop_duplicates(inplace=True)
-df.info()
+#df.info()
+#Renombrar columnas
+df.rename(columns={"Age":"Edad",
+                   "Gender":"Genero",
+                   "Heart rate":"Frecuencia cardiaca",
+                   "Systolic blood pressure":"Presion arterial sistolica",
+                   "Diastolic blood pressure":"Presion arterial diastolica",
+                   "Blood sugar":"Azucar en sangre",
+                   "Result":"Resultado"}, inplace=True)
+#df.info()
+#Tratado de atipicos
+def remover_atipicos(df, column):
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    limite_inf = Q1 - 1.5 * IQR
+    limite_super = Q3 + 1.5 * IQR
+    return df[(df[column] >= limite_inf) & (df[column] <= limite_super)]
+# Se crea un dataset nuevo para ver las diferencias entre los datos originales y los filtrados
+df_filtrado = df.copy()
+df_filtrado = remover_atipicos(df_filtrado, 'Frecuencia cardiaca')
+df_filtrado = remover_atipicos(df_filtrado, 'Presion arterial sistolica')
+df_filtrado = remover_atipicos(df_filtrado, 'Presion arterial diastolica')
+df_filtrado = remover_atipicos(df_filtrado, 'Azucar en sangre')
+df_filtrado.info()
+
